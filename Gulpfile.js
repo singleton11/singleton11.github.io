e0e05cb5a73089622d09e8ee92c3126f7ebbf267
Anton Prokhorov <betrayer11@gmail.com>
2017-01-19 18:39:54 +0700

    Restructuring of frontend
    To have an ability to run 'gulp' command from root of project
    Also added .python-version

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,26 +1,27 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 
-var paths = ['src/css/*', 'src/img/*'];
+var paths = ['assets/css/*', 'assets/img/*'];
 
 gulp.task('css', function () {
-    return gulp.src(['src/css/style.css', 'src/css/colorful.css', 'node_modules/bootstrap/dist/css/bootstrap.min.css'])
+    return gulp.src(['assets/css/style.css', 'assets/css/colorful.css', 'node_modules/bootstrap/dist/css/bootstrap.min.css'])
         .pipe(concat('all.css'))
-        .pipe(gulp.dest('build/css'));
+        .pipe(gulp.dest('themes/custom/static/css'));
 });
 
 gulp.task('js', function () {
     return gulp.src(['node_modules/jquery/dist/jquery.min.js', 'node_modules/bootstrap/dist/js/bootstrap.min.js'])
         .pipe(concat('all.js'))
-        .pipe(gulp.dest('build/js'));
+        .pipe(gulp.dest('themes/custom/static/js'));
 });
 
 gulp.task('fonts', function () {
-    return gulp.src(['node_modules/bootstrap/dist/fonts/*', 'src/fonts/*']).pipe(gulp.dest('build/fonts'));
+    return gulp.src(['node_modules/bootstrap/dist/fonts/*', 'src/fonts/*'])
+        .pipe(gulp.dest('themes/custom/static/fonts'));
 });
 
 gulp.task('images', function () {
-    return gulp.src('src/img/*').pipe(gulp.dest('build/img'));
+    return gulp.src('assets/img/*').pipe(gulp.dest('themes/custom/static/img'));
 });
 
 gulp.task('default', ['css', 'js', 'fonts', 'images']);
